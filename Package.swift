7ea2be88b304e2cebac117d416e1c85e49d420a8
Aravind G S <aravind.gs@whatfix.com>
2023-07-07 15:44:01 +0530

    LeapCoreSDK.xcframework.zip location correction

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(name: "LeapSDK", path: "LeapSDK.xcframework"),
         .binaryTarget(
             name: "LeapCoreSDK", 
-            url: "https://bitbucket.org/whatfix/core-ios/raw/04af09c459837c1d8cc1152e250d29358508bcab/LeapCoreSDK.xcframework.zip",
+            url: "https://bitbucket.org/whatfix/core-ios/downloads/LeapCoreSDK.xcframework.zip",
             checksum: "ff56dfc17e3665791b81bf6ad766cd402a1360fa460335fcf08ea1ea0692e079"
         )
     ]
